c830e92641a1ce7106187af40e7e67c9f2f90342
saritribanokta <65384859+saritribanokta@users.noreply.github.com>
2020-05-15 21:21:23 +0700

    Add files via upload

---
+++ saritri_grafik/config.php
@@ -0,0 +1,12 @@
+<?php
+
+$databaseHost='localhost';
+$databaseName='pmb';
+$databaseUsername='root';
+$databasePassword='';
+
+$conn=new mysqli($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+if($conn -> connect_errno){
+	echo die ("Gagal menghubungkan ke Database ".$conn->connect_error);
+}
+?>
